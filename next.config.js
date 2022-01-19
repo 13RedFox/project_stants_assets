6526f2b048c901a8f6dfa82b94f9586b61628505
Serhii <sergei181989@gmail.com>
2022-01-19 22:14:40 +0200

    :construction: Init project & refactor code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePath: [path.join(__dirname, 'src/scss')],
+  }
 }
 
 module.exports = nextConfig
